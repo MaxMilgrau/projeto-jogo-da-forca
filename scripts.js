8faaa4f62afd2fa78ec98666754c71ff6fe245a1
Gabriel <sejaomaximos@gmail.com>
2023-10-26 17:09:17 -0300

    protótipo finalizado

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const forca_container=document.querySelector('#forca-container');
+const button=document.querySelector('#enviar');
+
+
+function buttonTest(){
+
+forca_container.style.display="none";
+
+
+
+}
+
+button.addEventListener('click', buttonTest);
